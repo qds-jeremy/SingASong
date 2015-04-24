93f67a17d7d413fe6859502371db02ffa8214b30
QDS-Jeremy <jeremy@quodisys.com>
2015-04-24 15:56:04 +0700

    Initial Commit

---
+++ CustomVideoCamera2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomVideoCamera2
+//
+//  Created by Jeremy on 4/24/15.
+//  Copyright (c) 2015 Jeremy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CustomVideoCamera2Tests/CustomVideoCamera2Tests.m
@@ -0,0 +1,40 @@
+//
+//  CustomVideoCamera2Tests.m
+//  CustomVideoCamera2Tests
+//
+//  Created by Jeremy on 4/24/15.
+//  Copyright (c) 2015 Jeremy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface CustomVideoCamera2Tests : XCTestCase
+
+@end
+
+@implementation CustomVideoCamera2Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
